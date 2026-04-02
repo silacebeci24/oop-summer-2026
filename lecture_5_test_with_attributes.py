df62a39f88c58d01dbdef76df5fc7745b998e2c8
silacebeci24 <silacebeci275@gmail.com>
2026-04-02 11:00:35 +0200

    Update lecture_5_test_with_attributes.py

--- lecture_5_test_with_attributes.py
+++ lecture_5_test_with_attributes.py
@@ -1,1 +1,9 @@
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
+obj = Person("Sila", "Cebeci")
+
+print(obj.sıla)
+print(obj.cebeci)
