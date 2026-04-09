32c56c79bfc7cfff70228d3774fab4bff1bc7c31
silacebeci24 <silacebeci275@gmail.com>
2026-04-09 08:37:39 +0000

    36330

---
+++ 36330/test_06_class_with_constructor.py
@@ -0,0 +1,9 @@
+class StudentRecord:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+student1 = StudentRecord("Sila", "A")
+
+print(student1.name)
+print(student1.grade)
---
+++ 36330/test_06_empty_class.py
@@ -0,0 +1,10 @@
+class studentRecord;
+    pass
+student1 = StudentRecord()
+
+student1.name = "Sila"
+student1.grade = "A"
+
+print(student1.name)
+print(student1.grade)
+
